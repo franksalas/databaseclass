1051c0ad5ac3d591ffc867b5907accfaf2f8d08a
franksalas <franksalas@gmail.com>
2015-10-17 18:13:49 -0500

    2nd attempt

--- project/views.py
+++ project/views.py
@@ -162,3 +162,14 @@ def delete_entry(product_id):
     flash('The product was deleted.')
     return redirect(url_for('products'))
 
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
